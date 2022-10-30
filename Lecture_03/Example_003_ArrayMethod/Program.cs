a3f45c11afc8d63925bf3a795ae501658c6e9a70
SMV <kraft088@gmail.com>
2022-10-31 00:16:22 +1100

    Add Example 003

---
+++ Lecture_03/Example_003_ArrayMethod/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+1. Найти позиции минимального элемента в неотсортированной части массива.
+2. Произвести обмен этого значения со значением первой неотсортированной позиции.
+3. Повторять пока есть неотсортированные элементы.
+*/
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        System.Console.Write($"{array[i]} ");
+    }
+    System.Console.WriteLine();
+}
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
+        int temp = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temp;
+    }
+}
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
